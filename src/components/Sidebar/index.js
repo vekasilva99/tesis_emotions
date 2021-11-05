98fb7a7040d3ba2361db07fdfd896e4f8f0c5bd1
Valeska Silva <valeska.silva@wuhk.app>
2021-11-05 11:58:07 -0400

    Menu

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,12 +1,14 @@
 import React, { useEffect, useState } from "react";
 import "../../styles/components/__components-dir.scss";
-
+import menu from '../../assets/17704.png'
 const Sidebar = ({drawerToggleClickHandler,color}) => {
 
 
   return (
     <div className="sidebar-container" >
-        <div className="logo-container" style={{background:color ? color:""}} onClick={drawerToggleClickHandler}></div>
+        <div className="logo-container" style={{background:color ? color:""}} >
+        <img className="menu-image" src={menu} onClick={drawerToggleClickHandler}></img>
+        </div>
 
     </div>
   );
